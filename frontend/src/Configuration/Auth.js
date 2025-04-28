c5c24b3ebbbb90900950f3f820564d8800ff33b0
vittalreddy <chettukindavittalreddy@gmail.com>
2025-04-28 18:45:27 +0530

    Add frontend folder with new files

---
+++ frontend/src/Configuration/Auth.js
@@ -0,0 +1,10 @@
+
+import React from "react";
+ export const ApiUrl = "http://localhost:3000";  
+ export function Auth() {
+     return (
+       <div>
+         <h1>API URL is: {ApiUrl}</h1>
+       </div>
+     );
+   }
